2a376d303a9df877f9e98dc26d7785624cc5f944
radit <raditya@femaledaily.com>
2018-09-19 18:39:11 +0700

    Created socket initial connecton server-client

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const http    = require('http')
-const io      = require('socket.io')(http)
 
 const app = express()
 const server = http.createServer(app)
+const io = require('socket.io')(server)
 const PORT = 2323
+
+io.on("connection", (socket) => {
+    console.log("an user connected with ID " + socket.id)
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected, ID " + socket.id)
+    })
+})
 
 server.listen(PORT, () => {
     console.log("Server started on port ", PORT)
